95f9e172c4053c79a6cfdac9963ce034408f6fa6
walter gomero <walter.gomero@gmail.com>
2025-04-29 14:47:37 -0500

    updated middleware to include sessionCartId

--- auth.config.js
+++ auth.config.js
@@ -1,38 +1,10 @@
 import { NextAuthConfig } from 'next-auth';
 import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
 
 
 export const authConfig = {
   pages: {
     signIn: '/signin',
-  },
-  callbacks: {
-    authorized({ auth, request}) {
-               //check for session cart cookies
-    if(!request.cookies.get('sessionCartId'))
-        {
-            //generate new session cart id cookie
-            const sessionCartId = crypto.randomUUID();
-                  
-            //clone the req headers
-            const newrequestHeaders = new Headers(request.headers);
-          
-            //create new response and add the new headers
-            const response = NextResponse.next({
-                request: {
-                headers: newrequestHeaders
-                }
-            })
-            // set newly generated sessionCartId int the response cookies
-            response.cookies.set('sessionCartId', sessionCartId);
-            return response;
-        }
-    else {
-        return true;
-        }
-    },
-    
   },
   providers: [], // Add providers with an empty array for now
 };
--- middleware.js
+++ middleware.js
@@ -2,15 +2,34 @@
 import { NextResponse } from "next/server";
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
-import GenerateChartCookie from '@/lib/generateCartCookie';
 
 const {auth} = NextAuth(authConfig,);
 
 export default  NextAuth(authConfig).auth; 
 
 export async function middleware(request) {
+  
+  //sessionCartId
+  const sessionCartId = request.cookies.get('sessionCartId');
+  if(!sessionCartId) {
 
- 
+    //generate new session cart id cookie
+    const sessionCartId = crypto.randomUUID();
+                  
+    //clone the req headers
+    const newrequestHeaders = new Headers(request.headers);
+  
+    //create new response and add the new headers
+    const response = NextResponse.next({
+        request: {
+        headers: newrequestHeaders
+        }
+    })
+    // set newly generated sessionCartId int the response cookies
+    response.cookies.set('sessionCartId', sessionCartId);
+    return response;
+  }
+
 
   const {nextUrl} = request;
   const session = await auth();
